14850c15eb81e30254e8dab731feebc31e59874c
Jesus Bossa <x@jesusbossa.dev>
2022-10-03 22:52:55 -0500

    chore: init

---
+++ ext/MainPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Main.css';
+
+const MainPage = () => {
+  return (
+    <div className="container">
+      <main>
+        <h1>Hello wasp</h1>
+      </main>
+    </div>
+  );
+};
+export default MainPage;
