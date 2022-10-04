a60f20cdc0d0e620a028de48153316560912008c
Jesus Bossa <x@jesusbossa.dev>
2022-10-04 11:57:29 -0500

    chore: crud tasks

--- ext/MainPage.js
+++ ext/MainPage.js
@@ -1,13 +1,67 @@
 import React from 'react';
-import './Main.css';
+
+import updateTask from '@wasp/actions/updateTask';
+import createTask from '@wasp/actions/createTask';
+import getTasks from '@wasp/queries/getTasks';
+import { useQuery } from '@wasp/queries';
 
 const MainPage = () => {
+  const { data: tasks, isFetching, error } = useQuery(getTasks);
+
   return (
-    <div className="container">
-      <main>
-        <h1>Hello wasp</h1>
-      </main>
+    <div>
+      <NewTaskForm />
+
+      {tasks && <TasksList tasks={tasks} />}
+
+      {isFetching && 'Fetching...'}
+      {error && 'Error: ' + error}
     </div>
   );
 };
+
+const Task = (props) => {
+  const handleIsDoneChange = async (event) => {
+    try {
+      await updateTask({
+        taskId: props.task.id,
+        data: { isDone: event.target.checked },
+      });
+    } catch (error) {
+      window.alert('Error while updating task: ' + error.message);
+    }
+  };
+  return (
+    <div>
+      <input type="checkbox" id={props.task.id} checked={props.task.isDone} onChange={handleIsDoneChange} readonly />
+      {props.task.description}
+    </div>
+  );
+};
+
+const TasksList = (props) => {
+  if (!props.tasks?.length) return <span>No tasks</span>;
+  return props.tasks.map((task, idx) => <Task task={task} key={idx} />);
+};
+
+const NewTaskForm = (props) => {
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const description = event.target.description.value;
+      event.target.reset();
+      await createTask({ description });
+    } catch (err) {
+      window.alert('Error: ' + err.message);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input name="description" type="text" defaultValue="" />
+      <input type="submit" value="Create task" />
+    </form>
+  );
+};
+
 export default MainPage;
